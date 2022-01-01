788b0899f1b3a22e07c318cd35f81dfade6e991a
hardcorequal <hardcorequal@gmail.com>
2022-01-01 14:06:39 +0200

    feat: add download anki-words

--- components/topWords.tsx
+++ components/topWords.tsx
@@ -3,6 +3,20 @@ import {useTopWords} from "../hooks/useTopWords";
 import {moreUsedWordsCoefficient} from "../config";
 import {Button} from "@mui/material";
 
+
+
+const downloadObjectAsJson = (data: any, name: string) => {
+  const json = JSON.stringify(data);
+  const blob = new Blob([new Buffer(json)], {
+    type: 'application/json'
+  });
+
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.download = name + '.json';
+  link.click();
+};
+
 export function TopWords({value}: { value: string }) {
   const {orderedWords, moreUsedCountWords, totalCountWords} = useTopWords(value);
 
@@ -12,17 +26,15 @@ export function TopWords({value}: { value: string }) {
 
   return <>
     <Button variant="contained" onClick={() => {
-      const json = JSON.stringify(orderedWords);
-      const blob = new Blob([new Buffer(json)], {
-        type: 'application/json'
-      });
-
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = 'words.json';
-      link.click();
-
+      downloadObjectAsJson(orderedWords, 'words')
     }}>Download</Button>
+
+    <Button variant="contained" onClick={() => {
+      downloadObjectAsJson(
+        orderedWords.map((e) => ({front: e})),
+        'anki-words'
+      )
+    }}>Download for Anki</Button>
 
     <div style={{ margin: '5px', fontSize: '24px' }}>
       {moreUsedWordsCoefficient * 100}% is {moreUsedCountWords} from {totalCountWords} words
