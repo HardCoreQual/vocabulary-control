074c5b71385f2f0535bdf29081c741c4c561c171
hardcorequal <hardcorequal@gmail.com>
2022-01-09 08:57:34 +0200

    feat: add tRPC api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import {InsertText} from "../components/insertText";
 import {TopWords} from "../components/topWords";
+import {trpc} from "../hooks/trpc";
 
 export default function IndexPage() {
   const [text, setText] = useState('');
-
+  const hello = trpc.useQuery(['hello', { text: 'client' }]);
   return (
     <>
+      {hello.data?.greeting}
       <InsertText text={text} setText={setText}/>
       <TopWords value={text}/>
     </>
