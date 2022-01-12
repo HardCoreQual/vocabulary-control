f95d5adef334c386c190dc3809cbcdea53e1f7dd
hardcorequal <hardcorequal@gmail.com>
2022-01-12 23:16:41 +0200

    fix: not use removed trpc query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import React, {useState} from 'react';
 import {InsertText} from "../components/insertText";
 import {TopWords} from "../components/topWords";
-import {trpc} from "../hooks/trpc";
 import {signIn, useSession} from "next-auth/react"
 
 export default function IndexPage() {
   const [text, setText] = useState('');
-  const hello = trpc.useQuery(['hello', { text: 'client' }]);
 
   const {status} = useSession();
 
